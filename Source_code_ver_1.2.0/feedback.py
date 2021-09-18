275e8e6c48e67d0249865e5e6e1d4633d32f19b9
Sourav <73434809+loku-sama@users.noreply.github.com>
2021-09-18 22:57:48 +0530

    Updated Documentation with Docstrings

--- Source_code_ver_1.2.0/feedback.py
+++ Source_code_ver_1.2.0/feedback.py
@@ -13,21 +13,37 @@ import urllib.request  # Foe checking internet connection
 
 class Feedback:
     def __init__(self, login_id, password):
-        """ Feedback class constructor """
+        """  Feedback class constructor
+
+        :param login_id: eMail login id from which the feedback mail will be sent.
+        :type login_id: str
+        :param password: Password of the email
+        :type password: str
+        """
         self.login_id = login_id  # Log in ID of the mail account from which you want to send the Feedback
         self.password = password  # Password of the mail account from which you want to send the Feedback
         self.context = ssl.create_default_context()  # Creates SSL connection
 
     def send_feedback(self, user_name, user_email, user_feedback):
-        """" Function for sending the feedback """
+        """  Function for sending the feedback
+
+        :param user_name: Application username entered by user
+        :type user_name: str
+        :param user_email: Application user email entered by user
+        :type user_email: str
+        :param user_feedback: Application user feedback entered by user
+        :type user_feedback: str
+        :return: True or False
+        :rtype: bool
+        """
         try:
-            server = smtplib.SMTP("your SMTP server", 000)  # SMTP Server IP and Port of your provider
+            server = smtplib.SMTP("smtp.office365.com", 587)  # SMTP Server IP and Port of your provider
             server.ehlo()  # Optional
             server.starttls(context=self.context)  # Starts TLS handshake
             server.login(self.login_id, self.password)  # Signing to your account
             email_body = f"Name : {user_name}\nemail : {user_email}\nFeedback : {user_feedback}"
             SUBJECT = 'Feedback of Retirement Benefit Calculator ver 1.2.0'
-            TO = 'receiver email id'
+            TO = 'happysourav96@gmail.com'
             FROM = self.login_id  # Mail id from where feedback will be sent
             msg = MIMEText(email_body)  # Mail body
             msg['Subject'] = SUBJECT  # Mail Subject
@@ -42,7 +58,13 @@ class Feedback:
 
 
 def connect(host='http://google.com'):
-    """ Function for checking the Internet Connection """
+    """  Function for checking the Internet Connection
+
+    :param host: Web address for ping testing
+    :type host: str
+    :return: True or False
+    :rtype: bool
+    """
     try:
         urllib.request.urlopen(host)
         return True
@@ -51,7 +73,13 @@ def connect(host='http://google.com'):
 
 
 def check_mail(email):
-    """ Function for checking valid email user input """
+    """  Function for checking valid email user input
+
+    :param email: Application user email entered by user
+    :type email: str
+    :return: True or False
+    :rtype: bool
+    """
     if "@" in str(email) and "." in str(email):
         return True
     else:
